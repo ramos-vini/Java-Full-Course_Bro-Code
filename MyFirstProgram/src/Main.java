37aafffe97b5a70c634b89af7e87ec1e9eb1f73e
ramos-vini <ramoscontrole@gmail.com>
2023-03-29 19:33:40 +0200

    First Commit

---
+++ MyFirstProgram/src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// This is a Comment (ignored by the Compiler)
+		System.out.println("I love pizza");
+		System.out.print("It's really good!\n");
+		System.out.print("\t-Do you agree?");
+		
+		/*
+		 * This is
+		 * also a
+		 * Comment
+		 */
+	}
+
+}
