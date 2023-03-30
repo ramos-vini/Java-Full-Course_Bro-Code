9baf5b018e1baeec154ba97fd5082117c31367f3
ramos-vini <ramoscontrole@gmail.com>
2023-03-30 11:14:55 +0200

    Expressions

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -1,23 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		int friends = 10;
 		
-		System.out.println("Hello! What's your name?");
-		String name = scanner.nextLine();
+		friends += 5;
 		
-		System.out.println("Cool name! How old are you?");
-		int age = scanner.nextInt();
-		scanner.nextLine();
-		
-		System.out.println("And what's your favorite food?");
-		String food = scanner.nextLine();
-		
-		System.out.println("Your name is: " + name);
-		System.out.println("You are " + age + " years old");
-		System.out.println("And your favorite food is " + food);
+		System.out.println("I now have: " + friends + " friends");
 		
 	}
 
