ba69c4352044fe82bc6e8f77e5e32f2320dc807b
ramos-vini <ramoscontrole@gmail.com>
2023-04-03 23:00:08 +0200

    for loop

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -1,17 +1,11 @@
-import java.util.Scanner;
-
 public class Main {
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
 		
-		String name = "";
+		for(int i=10; i>0; i--) {
+			System.out.println(i);
+		}
 		
-		do {	
-			System.out.println("Hello! What's your name?");
-			name = scanner.nextLine();
-		} while(name == "");
-		
-		System.out.println("Nice to meet you, "+name+"!");
+		System.out.println("Happy New Year!");
 	}
 }
