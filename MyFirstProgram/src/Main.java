48b23281c68849ff133ca53d84c50c6573031702
ramos-vini <ramoscontrole@gmail.com>
2023-04-03 23:19:44 +0200

    Nested loops

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-		for(int i=10; i>0; i--) {
-			System.out.println(i);
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Let's build a rectangle");
+		
+		System.out.println("Enter # of rows");
+		int rows = scanner.nextInt();
+		
+		System.out.println("Enter # of columns");
+		int columns = scanner.nextInt();
+		
+		System.out.println("Enter a symbol");
+		String symbol = scanner.next();
+		
+		System.out.println("Great! Here's your rectangle:");
+		
+		for(int i=1; i<=rows; i++) {
+			System.out.println();
+			
+			for(int j = 1; j<=columns; j++) {
+				System.out.print(symbol);
+			}
 		}
 		
-		System.out.println("Happy New Year!");
 	}
 }
