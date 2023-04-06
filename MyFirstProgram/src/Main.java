e9e4ee93ccea9ea728aed9bea5cfc0075dca5725
ramos-vini <ramoscontrole@gmail.com>
2023-04-06 23:21:27 +0200

    2D ArrayList

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -1,23 +1,39 @@
-import java.util.ArrayList;
+import java.util.*;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		ArrayList<String> foods = new ArrayList<String>();
+		ArrayList<String> bakeryList = new ArrayList<String>();
 		
-		foods.add("sushi");
-		foods.add("hotdog");
-		foods.add("hamburger");
+		bakeryList.add("Croissant");
+		bakeryList.add("Garlic Bread");
+		bakeryList.add("Cheese Bread");
 		
-		foods.remove(0);
-		foods.set(1, "pizza");
-		foods.add(1, "döner");
-		// foods.clear();
+		ArrayList<String> produceList = new ArrayList<String>();
 		
+		produceList.add("Tomatoes");
+		produceList.add("Onions");
+		produceList.add("Pasta");
 		
-		for(int i=0; i<foods.size(); i++) {
-			System.out.println(foods.get(i));
+		ArrayList<String> saucesList = new ArrayList<String>();
+		
+		saucesList.add("Spicy");
+		saucesList.add("Rosé");
+		
+		ArrayList<ArrayList<String>> groceryList = new ArrayList<>(); // 2D ArrayList
+		
+		groceryList.add(bakeryList);
+		groceryList.add(produceList);
+		groceryList.add(saucesList);
+		
+		for(int i=0; i<groceryList.size(); i++) {
+			System.out.println("List "+(i+1)+":\n");
+			for(int j=0; j<groceryList.get(i).size(); j++) {
+				System.out.print("- ");
+				System.out.println(groceryList.get(i).get(j));
+			}
+			System.out.println();
 		}
 		
 	}
