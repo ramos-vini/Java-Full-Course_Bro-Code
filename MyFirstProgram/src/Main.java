766d9a99fb77d4846f442bac75aea74b96ed3733
ramos-vini <ramoscontrole@gmail.com>
2023-04-06 22:28:24 +0200

    String Methods

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -2,44 +2,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		int[][] numbers = new int[3][3];
+		String name = "Bro";
 		
-		numbers[0][0] = 1;
-		numbers[0][1] = 2;
-		numbers[0][2] = 3;
+		//boolean result = name.equals("bro");
+		//boolean result = name.equalsIgnoreCase("bro");
+		//char result = name.charAt(0);
+		//int result = name.indexOf("r");
+		//String result = name.replace("o", "a");
+		String result = name.trim();
 		
-		numbers[1][0] = 4;
-		numbers[1][1] = 5;
-		numbers[1][2] = 6;
-		
-		numbers[2][0] = 7;
-		numbers[2][1] = 8;
-		numbers[2][2] = 9;
-		
-		for (int i=0; i<numbers.length; i++) {
-			System.out.println();
-			
-			for (int j=0; j<numbers[i].length; j++) {
-				System.out.print(numbers[i][j] + " ");
-			}
-		}
-		
-		System.out.println();
-		System.out.print("- - -");
-		
-		char[][] letters = {
-				{'a', 'b', 'c'},
-				{'d', 'e', 'f'},
-				{'g', 'h', 'j'}
-		};
-		
-		for (int i=0; i<letters.length; i++) {
-			System.out.println();
-			
-			for (int j=0; j<letters[i].length; j++) {
-				System.out.print(letters[i][j] + " ");
-			}
-		}
+		System.out.println(result);
 		
 	}
 }
