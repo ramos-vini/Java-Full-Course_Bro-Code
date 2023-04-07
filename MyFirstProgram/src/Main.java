808df627aa928951ebd8c0a5c800b1f3176763a3
ramos-vini <ramoscontrole@gmail.com>
2023-04-07 13:16:11 +0200

    printf method

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -4,26 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		System.out.println(add(1, 2));
-	}
-	
-	static int add(int a, int b) {
-		System.out.println("This is overloaded method #1");
-		return a + b;
-	}
-	
-	static int add(int a, int b, int c) {
-		System.out.println("This is overloaded method #2");
-		return a + b + c;
-	}
-	
-	static String add(String a, String b) {
-		System.out.println("This is overloaded method #3");
-		return a + b;
-	}
-	
-	static double add(int a, int b, double c) {
-		System.out.println("This is overloaded method #4");
-		return a + b + c;
+		double number = 3250.1456789;
+		
+		System.out.printf("Here goes the variable: %,.2f", number);
 	}
 }
