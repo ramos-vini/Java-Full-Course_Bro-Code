07d1cf429cceda87703e84afdbf5e80ce48fa501
ramos-vini <ramoscontrole@gmail.com>
2023-04-07 12:47:52 +0200

    Overloaded Methods

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -3,22 +3,27 @@ import java.util.*;
 public class Main {
 
 	public static void main(String[] args) {
-		
-		String name = "Bro";
-		
-		hello(name, 21);
-		
-		System.out.println(add(2,3));
+	
+		System.out.println(add(1, 2));
 	}
 	
-	static void hello(String p1, int p2) {
-		System.out.println("Hello "+p1+", you're "+p2);
+	static int add(int a, int b) {
+		System.out.println("This is overloaded method #1");
+		return a + b;
 	}
 	
-	static int add(int p1, int p2) {
-		
-		int sum = p1+p2;
-		
-		return sum;
+	static int add(int a, int b, int c) {
+		System.out.println("This is overloaded method #2");
+		return a + b + c;
+	}
+	
+	static String add(String a, String b) {
+		System.out.println("This is overloaded method #3");
+		return a + b;
+	}
+	
+	static double add(int a, int b, double c) {
+		System.out.println("This is overloaded method #4");
+		return a + b + c;
 	}
 }
