badb9e4740c5ecf5d0fd64e58988ad95df178f31
ramos-vini <ramoscontrole@gmail.com>
2023-04-07 12:15:23 +0200

    for-each loop

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -4,37 +4,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		ArrayList<String> bakeryList = new ArrayList<String>();
+		String[] animals = {"cat", "dog", "bird", "fish"};
 		
-		bakeryList.add("Croissant");
-		bakeryList.add("Garlic Bread");
-		bakeryList.add("Cheese Bread");
-		
-		ArrayList<String> produceList = new ArrayList<String>();
-		
-		produceList.add("Tomatoes");
-		produceList.add("Onions");
-		produceList.add("Pasta");
-		
-		ArrayList<String> saucesList = new ArrayList<String>();
-		
-		saucesList.add("Spicy");
-		saucesList.add("Rosé");
-		
-		ArrayList<ArrayList<String>> groceryList = new ArrayList<>(); // 2D ArrayList
-		
-		groceryList.add(bakeryList);
-		groceryList.add(produceList);
-		groceryList.add(saucesList);
-		
-		for(int i=0; i<groceryList.size(); i++) {
-			System.out.println("List "+(i+1)+":\n");
-			for(int j=0; j<groceryList.get(i).size(); j++) {
-				System.out.print("- ");
-				System.out.println(groceryList.get(i).get(j));
-			}
-			System.out.println();
+		for(String animal : animals) {
+			System.out.println(animal);
 		}
 		
+		System.out.println("--------");
+		
+		ArrayList<String> sports = new ArrayList<>();
+		
+		sports.add("soccer");
+		sports.add("tennis");
+		sports.add("mma");
+		
+		for(String sport : sports) {
+			System.out.println(sports.indexOf(sport));
+		}
 	}
 }
