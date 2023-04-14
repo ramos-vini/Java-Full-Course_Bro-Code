3d38882a7b37a9a59f71e97bb57655595a0eb504
ramos-vini <ramoscontrole@gmail.com>
2023-04-14 09:13:11 +0200

    Object Oriented Programming (OOP)

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -2,10 +2,27 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		final double PI = 3.14159;
+		Car myCar1 = new Car();
 		
-		// pi = 4;
+		System.out.println(myCar1.make);
+		System.out.println(myCar1.model);
 		
-		System.out.println(PI);
+		System.out.println();
+		
+		myCar1.drive();
+		myCar1.brake();
+		
+		System.out.println("----------------------");
+		
+		Car myCar2 = new Car();
+		
+		System.out.println(myCar2.make);
+		System.out.println(myCar2.model);
+		
+		System.out.println();
+		
+		myCar2.drive();
+		myCar2.brake();
+		
 	}
 }
