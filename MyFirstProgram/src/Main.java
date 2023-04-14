dcd4c7d3fb0221d2e7f1941fb1d44aca4f48e462
ramos-vini <ramoscontrole@gmail.com>
2023-04-14 10:01:00 +0200

    Variable Scope

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -2,13 +2,9 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		Human human1 = new Human("Rick", 65, 70);
+		DiceRoller diceRoller = new DiceRoller();
 		
-		Human human2 = new Human("Morty", 16, 50.4);
-		
-		human2.eat();
-		
-		human1.drink();
+		//myDice.roll();
 		
 	}
 }
