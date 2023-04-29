4c15942c534c32d4e66e8da3d28d374c2056fe98
ramos-vini <ramoscontrole@gmail.com>
2023-04-29 10:43:00 +0200

    Array of Objects

---
+++ MyFirstProgram/src/Food.java
@@ -0,0 +1,10 @@
+
+public class Food {
+	String name;
+	int rating;
+	
+	Food(String name, int rating){
+		this.name = name;
+		this.rating = rating;
+	}
+}
--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -1,11 +1,23 @@
 public class Main {
 
 	public static void main(String[] args) {
-	
-		Car myCar = new Car();
 		
-		System.out.println(myCar); // Implicit
-		// System.out.println(myCar.toString()); -- Explicit
+		// Food[] refrigerator = new Food[3];
 		
+		Food food1 = new Food("Pizza", 7);
+		Food food2 = new Food("Sushi", 10);
+		Food food3 = new Food("Hamburguer", 9);
+		
+		Food[] refrigerator = {food1, food2, food3};
+		
+		for (Food food : refrigerator) {
+			System.out.printf("Food: %s\nRating: %d\n\n", food.name, food.rating);
+		}
+		
+		/*
+		System.out.printf("Food 1: %s\nRating: %d\n\n", food1.name, food1.rating);
+		System.out.printf("Food 2: %s\nRating: %d\n\n", food2.name, food2.rating);
+		System.out.printf("Food 3: %s\nRating: %d\n\n", food3.name, food3.rating);
+		*/
 	}
 }
