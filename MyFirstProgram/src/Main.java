f163e7c27406f60c4b6576e744e0008fd9062af4
ramos-vini <ramoscontrole@gmail.com>
2023-04-30 13:08:14 +0200

    abstraction

--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -2,10 +2,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Person person1 = new Person("Billy", 21);
-		System.out.println(person1.toString());
+		Car car = new Car();
 		
-		Hero hero1 = new Hero("Hulk", 40, "Strength");
-		System.out.println(hero1.toString());
+		car.go();
 	}
 }
