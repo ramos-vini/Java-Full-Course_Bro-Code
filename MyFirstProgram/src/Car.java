11329cac23763697d3f32b696bfa599f33f6b5ec
ramos-vini <ramoscontrole@gmail.com>
2023-05-01 11:50:15 +0200

    copy objects

--- MyFirstProgram/src/Car.java
+++ MyFirstProgram/src/Car.java
@@ -5,21 +5,24 @@ public class Car{
 	private String make;
 	private int year;
 	
+	// Constructor methods:
 	Car(String model, String make, int year){
 		this.model = model;
+		// or: this.setModel(model);
 		this.make = make;
 		this.year = year;
+	}
+	Car(Car copiedCar){
+		this.copy(copiedCar);
 	}
 	
 	// getter methods:
 	void setModel(String newModel) {
 		model = newModel;
 	}
-	
 	void setMake(String newMake) {
 		make = newMake;
 	}
-	
 	void setYear(int newYear) {
 		year = newYear;
 	}
@@ -27,14 +30,19 @@ public class Car{
 	// setter methods:
 	String getModel() {
 		return model;
-	}
-	
+	}	
 	String getMake() {
 		return make;
 	}
-	
 	int getYear() {
 		return year;
+	}
+	
+	// copy method
+	void copy(Car copiedCar) {
+		this.setMake(copiedCar.make);
+		this.setModel(copiedCar.model);
+		this.setYear(copiedCar.year);
 	}
 	
 }
--- MyFirstProgram/src/Main.java
+++ MyFirstProgram/src/Main.java
@@ -2,12 +2,24 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Car myCar = new Car("Camaro", "Chevrolet", 2020);
+		Car car1 = new Car("Camaro", "Chevrolet", 2020);
+		Car car2 = new Car ("Civic", "Honda", 2022);
 		
-		myCar.setYear(2022);
+		// car2 = car1; --> Wrong! (Shared memory address)
 		
-		System.out.println(myCar.getModel());
-		System.out.println(myCar.getMake());
-		System.out.println(myCar.getYear());
+		car2.copy(car1);
+		car2.setYear(2023);
+		//or: Car car2 = new Car(car1);
+		
+		System.out.println(car1);
+		System.out.println(car2);
+		System.out.println();
+		System.out.println(car1.getModel());
+		System.out.println(car1.getMake());
+		System.out.println(car1.getYear());
+		System.out.println();
+		System.out.println(car2.getModel());
+		System.out.println(car2.getMake());
+		System.out.println(car2.getYear());
 	}
 }
